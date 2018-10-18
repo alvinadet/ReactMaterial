50706bb8b00de3b99cb4115cbcd40a0d4b9b4e99
sodojiwo <alvinadetya@rocketmail.com>
2018-10-18 20:35:01 +0700

    bagaimana react bekerja

--- src/App.js
+++ src/App.js
@@ -5,23 +5,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Hallo</h1>
+        <h1>Hallo</h1>
+        <Header />
       </div>
     );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return <h1>Ini Header</h1>;
   }
 }
 
