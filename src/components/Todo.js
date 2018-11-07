9465b35b7d6165d5630ff1178866acc206acc646
sodojiwo <alvinadetya@rocketmail.com>
2018-11-07 21:38:15 +0700

    reactstrap

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,20 +1,104 @@
 import React, { Component } from 'react';
+import { Button, Input } from 'reactstrap';
+import './todo.css';
 
 class Todo extends Component {
   state = {
-    database: ['Makan', 'lari', 'olahraga', 'renang'],
-    input: ''
+    database: [],
+    input: '',
+    buttonView: false,
+    id: 0
   };
+
+  handleChange = e => {
+    this.setState({
+      input: e.target.value
+    });
+    console.log(this.state.input);
+  };
+
+  addData = () => {
+    const database = this.state.database;
+    const dataInput = this.state.input;
+    database.push(dataInput);
+    localStorage.setItem('todos', JSON.stringify(database));
+    this.setState({
+      input: ''
+    });
+  };
+
+  getData = () => {
+    let getItem = JSON.parse(localStorage.getItem('todos'));
+    if (getItem !== null) {
+      return this.setState({
+        database: getItem
+      });
+    }
+  };
+
+  getDataId = index => {
+    const database = this.state.database;
+    const getData = database[index];
+    this.setState({
+      input: getData,
+      buttonView: true,
+      id: index
+    });
+  };
+
+  editdata = id => {
+    const database = this.state.database;
+    database.splice(id, 1, this.state.input);
+    localStorage.setItem('todos', JSON.stringify(database));
+    this.getData();
+    this.setState({
+      input: '',
+      buttonView: false
+    });
+  };
+  buttonCancel = () => {
+    this.setState({
+      buttonView: false
+    });
+  };
+  deleteData = index => {
+    const database = this.state.database;
+    database.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(database));
+    this.getData();
+  };
+  componentDidMount() {
+    this.getData();
+  }
 
   render() {
     return (
-      <div>
+      <div className="backgroundtodo">
         <h1>ToDo</h1>
-        <input type="text" /> <br />
-        <button>Simpan</button>
+        <Input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.input}
+        />
+        <br />
+        {this.state.buttonView ? (
+          <div>
+            <button onClick={() => this.editdata(this.state.id)}>Edit</button>
+            <button onClick={() => this.buttonCancel()}>Batal</button>
+          </div>
+        ) : (
+          <Button onClick={() => this.addData()}>Simpan</Button>
+        )}
+
         <ul>
-          {this.state.database.map(datum => {
-            return <li>{datum}</li>;
+          {this.state.database.map((datum, index) => {
+            return (
+              <div>
+                <li>{datum}</li>
+                <button onClick={() => this.deleteData(index)}>Hapus</button>
+                <button onClick={() => this.getDataId(index)}>Edit</button>
+              </div>
+            );
           })}
         </ul>
       </div>
